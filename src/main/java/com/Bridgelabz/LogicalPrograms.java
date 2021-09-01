e3266a741580c3522b33811e28fa6375f6745963
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 11:16:35 +0530

    Added: Reverse_Number

--- src/main/java/com/Bridgelabz/LogicalPrograms.java
+++ src/main/java/com/Bridgelabz/LogicalPrograms.java
@@ -5,21 +5,16 @@ public class LogicalPrograms {
     public static void main(String[] args){
        // System.out.println("Logical Programming");
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a number ");
+        System.out.println("Enter a 4 number to reverse");
         int num =sc.nextInt();
-        int flag=1;
-        if(num==1||num==2||num==3)
-            System.out.println("The number is Prime number");
-        else {
-            for (int i = 2; i <num; i++) {
-                if (num % i ==0 )
-                    flag = 0;
-            }
-            if (flag == 1)
-                System.out.println("The number is a Prime number");
-            else
-                System.out.println("The number is not a Prime number");
+        int newnum=0;
+        while(num!= 0) {
+            newnum = newnum * 10;
+            newnum = newnum+ num%10;
+            num = num / 10;
         }
+        System.out.println("\nThe reversed number is "+newnum);
+
     }
 }
 
