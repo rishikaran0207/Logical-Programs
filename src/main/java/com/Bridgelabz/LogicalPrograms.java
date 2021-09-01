863dc4c780ce525076cfed9d181de85bc238a8f6
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 10:52:59 +0530

    Added: Perfect Number

--- src/main/java/com/Bridgelabz/LogicalPrograms.java
+++ src/main/java/com/Bridgelabz/LogicalPrograms.java
@@ -1,18 +1,23 @@
 package com.Bridgelabz;
 import java.util.Scanner;
 public class LogicalPrograms {
+    public static int total =0;
     public static void main(String[] args){
        // System.out.println("Logical Programming");
-        System.out.println(" Fibonnacci ");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Type ' s ' to Start :");
-        long start = System.currentTimeMillis();
-        sc.nextLine();
-        System.out.print("Type ' p ' to Stop :");
-        sc.nextLine();
-        long stop = System.currentTimeMillis();
-        long Elapsedtime = (stop - start) / 1000;
-        System.out.print("Elapsed Time was: "+Elapsedtime+" Seconds");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num =sc.nextInt();
+
+        for (int i=1;i<num;i++) {
+            if (num % i == 0) {
+                total = total + i;
+            }
+
+        }
+        if(total == num)
+            System.out.println("The number "+num+" is a perfect number.");
+        else
+            System.out.println("The number "+num+" is not a perfect number.");
     }
 }
 
