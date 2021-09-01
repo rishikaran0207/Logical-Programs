0cee6b980871fae268e37c6968d7d7100b8724ab
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 11:12:52 +0530

    Added: Prime_Number

--- src/main/java/com/Bridgelabz/LogicalPrograms.java
+++ src/main/java/com/Bridgelabz/LogicalPrograms.java
@@ -1,23 +1,25 @@
 package com.Bridgelabz;
 import java.util.Scanner;
 public class LogicalPrograms {
-    public static int total =0;
+
     public static void main(String[] args){
        // System.out.println("Logical Programming");
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number ");
         int num =sc.nextInt();
-
-        for (int i=1;i<num;i++) {
-            if (num % i == 0) {
-                total = total + i;
+        int flag=1;
+        if(num==1||num==2||num==3)
+            System.out.println("The number is Prime number");
+        else {
+            for (int i = 2; i <num; i++) {
+                if (num % i ==0 )
+                    flag = 0;
             }
-
+            if (flag == 1)
+                System.out.println("The number is a Prime number");
+            else
+                System.out.println("The number is not a Prime number");
         }
-        if(total == num)
-            System.out.println("The number "+num+" is a perfect number.");
-        else
-            System.out.println("The number "+num+" is not a perfect number.");
     }
 }
 
