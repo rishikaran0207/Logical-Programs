900d9b45d873ad94ef2ab30ea0b8d631d115da35
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 10:46:41 +0530

    Added: Fibonacci

--- src/main/java/com/Bridgelabz/LogicalPrograms.java
+++ src/main/java/com/Bridgelabz/LogicalPrograms.java
@@ -1,8 +1,18 @@
 package com.Bridgelabz;
-
+import java.util.Scanner;
 public class LogicalPrograms {
     public static void main(String[] args){
-        System.out.println("Logical Programming");
+       // System.out.println("Logical Programming");
+        System.out.println(" Fibonnacci ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type ' s ' to Start :");
+        long start = System.currentTimeMillis();
+        sc.nextLine();
+        System.out.print("Type ' p ' to Stop :");
+        sc.nextLine();
+        long stop = System.currentTimeMillis();
+        long Elapsedtime = (stop - start) / 1000;
+        System.out.print("Elapsed Time was: "+Elapsedtime+" Seconds");
     }
-
 }
+
