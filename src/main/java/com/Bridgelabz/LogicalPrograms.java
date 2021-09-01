f71081fb8a4258bc82a0afdf9423ee4f35fdf4e6
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-01 11:44:58 +0530

    Added: Stopwatch

--- src/main/java/com/Bridgelabz/LogicalPrograms.java
+++ src/main/java/com/Bridgelabz/LogicalPrograms.java
@@ -1,18 +1,23 @@
 package com.Bridgelabz;
 import java.util.Scanner;
 public class LogicalPrograms {
-    public static void main(String[] args){
-       // System.out.println("Logical Programming");
-        System.out.println(" Fibonnacci ");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Type ' s ' to Start :");
-        long start = System.currentTimeMillis();
-        sc.nextLine();
-        System.out.print("Type ' p ' to Stop :");
-        sc.nextLine();
-        long stop = System.currentTimeMillis();
-        long Elapsedtime = (stop - start) / 1000;
-        System.out.print("Elapsed Time was: "+Elapsedtime+" Seconds");
+    public static int num,len;
+    public static void main(String[] args) {
+        Scanner fi = new Scanner(System.in);
+        System.out.println("Enter the no of terms to find fibonacci series:");
+        int n = fi.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        System.out.println("The Fibonacci Series for "+n+" terms are");
+        System.out.println(n1);
+        System.out.println(n2);
+        for (int i = 2; i <n;++i) {
+            int n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.println(n3);
+        }
+
     }
 }
 
